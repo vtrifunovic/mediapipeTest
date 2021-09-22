9723bbacdab10e8cca8b7edb22fa7460fed4fe47
vtrifunovic <adatrifunovic@gmail.com>
2021-09-21 23:00:48 -0500

    controls test

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,3 +1,4 @@
+import math
 import cv2
 import mediapipe as mp
 import time
@@ -13,6 +14,7 @@ class handDetector():
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode,self.maxHands,self.detectionCon,self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
+        self.tipIDS = [4, 8, 12, 16, 20]
 
     def findHands(self, img, draw=True):
             imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
@@ -25,14 +27,14 @@ class handDetector():
             return img
 
     def findPosition(self, img, handNo=0,draw=True):
-        lmList = []
-        if self.results.multi_handedness:
-            label = self.results.multi_handedness[handNo].classification[0].label  # label gives if hand is left or right
-            #account for inversion in webcams
-            if label == "Left":
-                label = "Right"
-            elif label == "Right":
-                label = "Left"
+        self.lmList = []
+        # if self.results.multi_handedness:
+        # label = self.results.multi_handedness[handNo].classification[0].label  # label gives if hand is left or right
+        #    #account for inversion in webcams
+        #    if label == "Left":
+        #        label = "Right"
+        #    elif label == "Right":
+        #        label = "Left"
                 
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
@@ -40,10 +42,10 @@ class handDetector():
                 #print(id,lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x*w), int(lm.y*h)
-                lmList.append([id, cx, cy, label])
+                self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx,cy), 7, (236,252,3))
-        return lmList
+        return self.lmList
     
     def amountHands(self, img):
         amount = 0
@@ -52,6 +54,43 @@ class handDetector():
                 #print("Hand")
                 amount += 1
         return amount
+
+    def fingersUp(self):
+        fingers = []
+        rightHand = False
+        if self.lmList[5][1] > self.lmList[17][1]:
+            rightHand = True
+        if rightHand:
+            if self.lmList[self.tipIDS[0]][1] > self.lmList[self.tipIDS[0] - 1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        else:
+            if self.lmList[self.tipIDS[0]][1] < self.lmList[self.tipIDS[0] - 1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        for id in range(1,5):
+            if self.lmList[self.tipIDS[id]][2] < self.lmList[self.tipIDS[id]-2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
+
+    def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1+x2)//2, (y1+y2)//2
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (255, 0, 255), cv2.FILLED)
+        length = math.hypot(x2-x1, y2-y1)
+        return length, img, [x1, y1, x2, y2, cx, cy]
+
+
 
 def main():
     
--- virtualkeyboard.py
+++ virtualkeyboard.py
@@ -6,8 +6,8 @@ import keyboard
 cap = cv2.VideoCapture(0)
 
 detector = htm.handDetector(detectionCon=0.75)
-tipIDS = [4,8,12,16,20]
-
+tipIDS = [4, 8, 12, 16, 20]
+wBox = (50, 100), (50, 100)
 #W,S,A,D
 #R, esc, space
 #1,2,3,4
@@ -16,33 +16,40 @@ while True:
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=True)
     if len(lmList) != 0:
-        for id in range(1,5):
-            if (lmList[tipIDS[id]][1] > 50 and lmList[tipIDS[id]][1] < 100) and (lmList[tipIDS[id]][2] > 50 and lmList[tipIDS[id]][2] < 100):
+        for id in range(1, 5):
+            if (lmList[tipIDS[id]][1] > 50 and lmList[tipIDS[id]][1] < 150) and (lmList[tipIDS[id]][2] > 50 and lmList[tipIDS[id]][2] < 150):
                 print("W")
-                keyboard.send('k')
-                #time.sleep(0.5)
-            elif (lmList[tipIDS[id]][1] > 100 and lmList[tipIDS[id]][1] < 150) and (lmList[tipIDS[id]][2] > 50 and lmList[tipIDS[id]][2] < 100):
+                keyboard.press('w')
+                time.sleep(0.1)
+            elif (lmList[tipIDS[id]][1] > 150 and lmList[tipIDS[id]][1] < 250) and (lmList[tipIDS[id]][2] > 50 and lmList[tipIDS[id]][2] < 150):
                 print("S")
-                keyboard.send('i')
-                #time.sleep(0.5)
-            elif (lmList[tipIDS[id]][1] > 150 and lmList[tipIDS[id]][1] < 200) and (lmList[tipIDS[id]][2] > 50 and lmList[tipIDS[id]][2] < 100):
+                keyboard.press('s')
+                time.sleep(0.1)
+            elif (lmList[tipIDS[id]][1] > 250 and lmList[tipIDS[id]][1] < 350) and (lmList[tipIDS[id]][2] > 50 and lmList[tipIDS[id]][2] < 150):
                 print("A")
-                keyboard.send('l')
-                #time.sleep(0.5)
-            elif (lmList[tipIDS[id]][1] > 200 and lmList[tipIDS[id]][1] < 250) and (lmList[tipIDS[id]][2] > 50 and lmList[tipIDS[id]][2] < 100):
+                keyboard.press('a')
+                time.sleep(0.1)
+            elif (lmList[tipIDS[id]][1] > 350 and lmList[tipIDS[id]][1] < 450) and (lmList[tipIDS[id]][2] > 50 and lmList[tipIDS[id]][2] < 150):
                 print("D")
-                keyboard.send('m')
-                #time.sleep(0.5)
-            elif (lmList[tipIDS[id]][1] > 250 and lmList[tipIDS[id]][1] < 300) and (lmList[tipIDS[id]][2] > 50 and lmList[tipIDS[id]][2] < 100):
+                keyboard.press('d')
+                time.sleep(0.1)
+            elif (lmList[tipIDS[id]][1] > 450 and lmList[tipIDS[id]][1] < 550) and (lmList[tipIDS[id]][2] > 50 and lmList[tipIDS[id]][2] < 150):
                 print("R")
-                keyboard.send('e')
-                #time.sleep(0.5)
+                keyboard.press('r')
+                time.sleep(0.1)
+            else:
+                keyboard.release('w')
+                keyboard.release('s')
+                keyboard.release('a')
+                keyboard.release('d')
+                keyboard.release('r')
 
-    cv2.rectangle(img, (50,50),  (100,100), (255,255,255), 2)
-    cv2.rectangle(img, (100,50), (150,100), (255,255,255), 2)
-    cv2.rectangle(img, (150,50), (200,100), (255,255,255), 2)
-    cv2.rectangle(img, (200,50), (250,100), (255,255,255), 2)
+    cv2.rectangle(img, (50,50),  (150,150), (255,255,255), 2)
+    cv2.rectangle(img, (150,50), (250,150), (255,255,255), 2)
+    cv2.rectangle(img, (250,50), (350,150), (255,255,255), 2)
+    cv2.rectangle(img, (350,50), (450,150), (255,255,255), 2)
+    cv2.rectangle(img, (450,50), (550,150), (255,255,255), 2)
 
-    cv2.imshow('Image',img)
+    cv2.imshow('Image', img)
     if cv2.waitKey(1) == ord('q'):
             break
